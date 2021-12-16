ccedd5c150328c8049fdcfb6c7ad8661d303041a
Jackson Price <59149601+jprice8@users.noreply.github.com>
2021-12-16 15:20:10 -0600

    Solve construct BST.

---
+++ arrays/amazonFreshPromo.py
@@ -0,0 +1,33 @@
+
+
+def isWinner(codeList, shoppingCart):
+    # Sliding window
+    codeIndex = 0
+    # Iterate through each sublist in codelist
+    while codeIndex < len(codeList):
+        subList = codeList[codeIndex]
+        result = checkForSublist(subList, shoppingCart)
+
+        # If sublist is not in big list
+        
+        codeIndex += 1
+
+    
+    
+def checkForSublist(subList, bigList):
+    """
+    Sliding window helper method to see if a sublist is in
+    a bigger list.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    codeList = [
+        ['apple', 'apple'],
+        ['banana', 'anything', 'banana']
+    ]
+
+    shoppingCart = ['orange', 'apple', 'apple', 'banana', 'orange', 'banana']
+
+    print(isWinner(codeList, shoppingCart))
--- trees/findClosestValueInBst.py
+++ trees/findClosestValueInBst.py
@@ -3,22 +3,21 @@ from typing import List
 
 
 def findClosestValueInBst(tree, target):
-    pass
+    return findClosestValueInBstHelper(tree, target, tree.value)
 
-# def inOrderTraversal(root) -> List[int]:
-#     """
-#     Traverse the tree given the root.
-#     """
-#     res = []
-#     if root:
-#         # Traverse the left branch first
-#         res = inOrderTraversal(root.left)
-#         # Append value to result array
-#         res.append(root.value)
-#         # Traverse right branch next
-#         res = res + inOrderTraversal(root.right)
+def findClosestValueInBstHelper(tree, target, closest):
+    currentNode = tree 
 
-#     return res
+    while currentNode is not None:
+        if abs(target - closest) > abs(target - currentNode.value):
+            closest = currentNode.value
+        if target < currentNode.value:
+            currentNode = currentNode.left 
+        elif target > currentNode.value:
+            currentNode = currentNode.right 
+        else:
+            break
+    return closest
 
 
 class BST:
@@ -28,21 +27,14 @@ class BST:
         self.right = None
 
 
-class TestProgram(unittest.TestCase):
-    def test_case_1(self):
-        root = BST(10)
-        root.left = BST(5)
-        root.left.left = BST(2)
-        root.left.left.left = BST(1)
-        root.left.right = BST(5)
-        root.right = BST(15)
-        root.right.left = BST(13)
-        root.right.left.right = BST(14)
-        root.right.right = BST(22)
-        expected = 13
-        actual = findClosestValueInBst(root, 12)
-        self.assertEqual(expected, actual)
-
-
 if __name__ == '__main__':
-    unittest.main()
+    root = BST(10)
+    root.left = BST(5)
+    root.left.left = BST(2)
+    root.left.left.left = BST(1)
+    root.left.right = BST(5)
+    root.right = BST(15)
+    root.right.left = BST(13)
+    root.right.left.right = BST(14)
+    root.right.right = BST(22)
+    print(findClosestValueInBst(root, 12))
