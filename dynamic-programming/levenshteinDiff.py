050ca7cc7b27a149ac4830cc481275a5f0c7dcae
Jackson Price <59149601+jprice8@users.noreply.github.com>
2021-12-27 14:20:16 -0600

    Work on levenshteindiff.

---
+++ dynamic-programming/levenshteinDiff.py
@@ -0,0 +1,20 @@
+
+
+def dirtyLevenshtein(str1, str2):
+    edits = [[x for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
+
+    # Replace 0 column with range to len(str2) + 1
+    for i in range(1, len(str2) + 1):
+        edits[i][0] = edits[i - 1][0] + 1
+
+    for i in range(1, len(str2) + 1):
+        for j in range(1, len(str1) + 1):
+            if str2[i - 1] == str1[j - 1]:
+                edits[i][j] = edits[i - 1][j - 1]
+            else:
+                edits[i][j] = 1 + min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1])
+
+    return edits[-1][-1]
+
+
+print(dirtyLevenshtein('abc', 'yabc'))
