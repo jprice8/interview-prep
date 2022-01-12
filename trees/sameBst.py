c20f0d4096d8bf7698bbabdf366ee7c880854d6e
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-01-12 17:04:45 -0600

    Solve same bst problem.

---
+++ trees/sameBst.py
@@ -0,0 +1,43 @@
+def sameBst(arrayOne, arrayTwo):
+    if len(arrayOne) != len(arrayTwo):
+        return False 
+
+    if len(arrayOne) == 0 and len(arrayTwo) == 0:
+        return True 
+
+    if arrayOne[0] != arrayTwo[0]:
+        return False 
+
+    leftOne = getSmaller(arrayOne)
+    leftTwo = getSmaller(arrayTwo)
+    rightOne = getBiggerOrEqual(arrayOne)
+    rightTwo = getBiggerOrEqual(arrayTwo)
+
+    return sameBst(leftOne, leftTwo) and sameBst(rightOne, rightTwo)
+
+
+def getSmaller(array):
+    smaller = []
+    for i in range(1, len(array)):
+        if array[i] < array[0]:
+            smaller.append(array[i])
+    return smaller 
+
+
+def getBiggerOrEqual(array):
+    biggerOrEqual = []
+    for i in range(1, len(array)):
+        if array[i] >= array[0]:
+            biggerOrEqual.append(array[i])
+    return biggerOrEqual
+
+
+if __name__ == '__main__':
+    # arrayOne = [10, 15, 8, 12, 94, 81, 5, 2, -1, 101, 45, 12, 8, -1, 8, 2, -34]
+    # arrayTwo = [10, 8, 5, 15, 2, 12, 94, 81, -1, -1, -34, 8, 2, 8, 12, 45, 100]
+
+    # arrayOne = [5, 2, -1, 100, 45, 12, 8, -1, 8, 10, 15, 8, 12, 94, 81, 2, -34]
+    # arrayTwo = [5, 8, 10, 15, 2, 8, 12, 45, 100, 2, 12, 94, 81, -1, -1, -34, 8]
+    arrayOne = [10, 15, 8, 12, 94, 81, 5, 2, 11]
+    arrayTwo = [10, 8, 5, 15, 2, 12, 11, 94, 81]
+    print(sameBst(arrayOne, arrayTwo))
