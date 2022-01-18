ddd8dc9cedd6fa4d7255b6695bc4f949275673d0
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-01-18 17:10:01 -0600

    Start longest increasing subsequence.

---
+++ dynamic-programming/longestIncreasingSubsequence.py
@@ -0,0 +1,16 @@
+def longestIncreasingSubsequence(array):
+    result = []
+    longestSub = recursiveHelper(array, result)
+    return longestSub
+
+
+def recursiveHelper(array, result):
+    if len(array) == 0:
+        return
+
+    # 
+
+
+if __name__ == '__main__':
+    array = [5, 7, -24, 12, 10, 2, 3, 12, 5, 6, 35] # [-24, 2, 3, 5, 6, 35]
+    print(longestIncreasingSubsequence(array))
