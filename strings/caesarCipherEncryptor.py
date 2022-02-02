d2f72a76bcac9936a2d9ed0c3b38c77f62814a56
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-02-02 14:20:37 -0600

    Solve caesar cipher.

--- strings/caesarCipherEncryptor.py
+++ strings/caesarCipherEncryptor.py
@@ -1,6 +1,15 @@
-def caesarCipher(string):
-    pass 
+def caesarCipher(string, key):
+    charArray = []
+    asciiConstant = 97 
+    for char in string:
+        asciiCode = ord(char)
+        caesarCode = asciiCode - asciiConstant
+        shiftedCode = (caesarCode + key) % 26 
+        newAsciiCode = shiftedCode + asciiConstant
+        shiftedInteger = chr(newAsciiCode)
+        charArray.append(shiftedInteger)
+    return ''.join(charArray)
 
 
 if __name__ == '__main__':
-    print(caesarCipher('hello'))
+    print(caesarCipher('xyz', 2))
