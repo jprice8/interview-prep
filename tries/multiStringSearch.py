cdb58c2522615df55b5e0f7929b385acf509c945
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-02-05 15:42:56 -0600

    Start multi string search for trie problem.

---
+++ tries/multiStringSearch.py
@@ -0,0 +1,8 @@
+def multiStringSearch(bigString, smallStrings):
+    pass 
+
+
+if __name__ == '__main__':
+    bigString = "this is the big string"
+    smallStrings = ["this", "yo", "is", "a", "bigger", "string", "kappa"]
+    print(multiStringSearch(bigString, smallStrings))
