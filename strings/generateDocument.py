834d5750317721c41389a89505b72cf005ed5c7c
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-02-07 13:59:02 -0600

    Sovle generate document.py

---
+++ strings/generateDocument.py
@@ -0,0 +1,12 @@
+def generateDocumentHashmap(characters, document):
+    pass 
+
+
+def generateDocumentConstantSpace(characters, document):
+    pass
+
+
+if __name__ == '__main__':
+    characters = "abcabc"
+    document = "aabbccc"
+    print(generateDocumentConstantSpace(characters, document))
