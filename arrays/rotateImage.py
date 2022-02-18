7c4384dc969f4beb577975d4860553ef6df3b0a5
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-02-18 15:40:25 -0600

    Solve rotate image.

---
+++ arrays/rotateImage.py
@@ -0,0 +1,42 @@
+def rotateImage1(matrix):
+    n = len(matrix[0])
+
+    for i in range(n // 2 + n % 2):
+        for j in range(n // 2):
+            tmp = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = matrix[i][j]
+            matrix[i][j] = tmp
+
+
+# O(m) time | O(1) space - where m is the number of cells in the matrix.
+class Solution:
+    def rotate(self, matrix):
+        self.transpose(matrix)
+        self.reflect(matrix)
+
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25],
+    ]
+    # print(rotateImage1(matrix)) # [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+    solution = Solution()
+    print(solution.rotate(matrix))
