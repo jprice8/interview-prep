55c4ab60650d2b938f473e83c318d6e1fb39bc14
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-02-20 15:24:04 -0600

    Start work on longest palindrome substring.

---
+++ dynamic-programming/longestPalindromicSubstring.py
@@ -0,0 +1,9 @@
+def longestPalindrome(string):
+    pass 
+
+
+if __name__ == '__main__':
+    string = 'babad'
+    print(longestPalindrome(string)) # bab or aba
+    string2 = 'cbbd'
+    # print(longestPalindrome(string2)) # bb
