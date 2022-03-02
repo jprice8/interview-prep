1b622b818c70c01312318c814c69c1da8733dc0a
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-03-01 18:50:31 -0600

    Solve search in sorted array.

--- arrays/searchInSortedArray.py
+++ arrays/searchInSortedArray.py
@@ -5,20 +5,38 @@ class Solution:
         while left <= right:
             mid = (left + right) // 2
 
-            if target == nums[mid]:
-                return mid
+            if nums[mid] == target:
+                return mid 
 
-            if nums[left] <= nums[mid]:
-                if target > nums[mid] or target < nums[left]:
-                    left = mid + 1
-                else:
+            # left is sorted
+            elif nums[left] <= nums[mid]:
+                # Is target on this side?
+                if nums[left] <= target < nums[mid]:
+                    # yee
                     right = mid - 1
+                else:
+                    # no
+                    left = mid + 1
 
-            # right sorted portion
+            # right is sorted
             else:
-                if target < nums[mid] or target > nums[right]:
-                    right = mid - 1
-                else:
+                # Is target on this side?
+                if nums[mid] < target <= nums[right]:
+                    # yes
                     left = mid + 1
+                else:
+                    # no
+                    right = mid - 1
 
-        return - 1
+        return -1
+
+
+
+if __name__ == '__main__':
+    # nums = [1, 2, 3, 4, 5]
+    # nums = [2, 3, 4, 5, 1]
+    # nums = [3, 4, 5, 1, 2]
+    # nums = [4, 5, 1, 2, 3]
+    nums = [4, 5, 6, 7, 0, 1, 2]
+    s = Solution()
+    print(s.searchInSortedArray(nums, 3))
