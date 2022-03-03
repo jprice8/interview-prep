96f03b26fd1d8d5a3e77661590a546dc50dadb60
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-03-03 11:40:55 -0600

    Find optimal solution for longest substring without repeating chars.

--- arrays/longestSubstringWithoutRepeating.py
+++ arrays/longestSubstringWithoutRepeating.py
@@ -1,18 +1,25 @@
-# Naive O(n^2)
-def longest_substring_without_repeating(string):
-    longest = 0
+class Solution:
+    def longestSubstring(self, s):
+        def get_length(l, r):
+            return r - l + 1
+        char_set = set()
+        res = 0
+        left = 0
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
 
-    for start in range(len(string)):
-        for end in range(len(string)):
-            sub = string[start : end + 1]
-            if len(set(sub)) == len(sub):
-                longest = max(longest, end + 1 - start)
+            char_set.add(s[right])
+            res = max(res, get_length(left, right))
 
-    return longest
+        return res
 
+            
 
 
 
 if __name__ == '__main__':
     string = 'abcdbea'
-    print(longest_substring_without_repeating(string))
+    s = Solution()
+    print(s.longestSubstring(string))
