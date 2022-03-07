a93b35536706830af49564277b2a3bb2f6f5fca0
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-03-07 14:36:39 -0600

    Solve invert btree.

--- trees/invertBinaryTree.py
+++ trees/invertBinaryTree.py
@@ -1,3 +1,7 @@
+import collections
+from nodeDepths import BinaryTreeNode
+
+
 def invertBinaryTree(tree):
     if tree is None:
         return 
@@ -11,6 +15,25 @@ def invertBinaryTree(tree):
     invertBinaryTree(tree.left)
     invertBinaryTree(tree.right)
 
+class Solution:
+    def invertTree(self, root):
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            node = q.popleft()
+            if node is None:
+                break
+
+            for child in [node.right, node.left]:
+                q.append(child)
+
+            tmpLeft = node.left
+            node.left = node.right
+            node.right = tmpLeft
+
+        return root
+
 
 class BinaryTree:
     def __init__(self, value):
@@ -20,26 +43,28 @@ class BinaryTree:
 
 
 if __name__ == '__main__':
-    node1 = BinaryTree(1)
-    node2 = BinaryTree(2)
-    node3 = BinaryTree(3)
+    node1 = BinaryTreeNode(1)
+    node2 = BinaryTreeNode(2)
+    node3 = BinaryTreeNode(3)
 
     node1.left = node2 
     node1.right = node3
 
-    node4 = BinaryTree(4)
-    node5 = BinaryTree(5)
-    node6 = BinaryTree(6)
+    node4 = BinaryTreeNode(4)
+    node5 = BinaryTreeNode(5)
+    node6 = BinaryTreeNode(6)
     node2.left = node4
     node2.right = node5
 
-    node7 = BinaryTree(7)
-    node8 = BinaryTree(8)
-    node9 = BinaryTree(9)
+    node7 = BinaryTreeNode(7)
+    node8 = BinaryTreeNode(8)
+    node9 = BinaryTreeNode(9)
     node3.left = node6
     node3.right = node7 
 
     node4.left = node8 
     node4.right = node9
 
-    print(invertBinaryTree(node1))
+    # print(invertBinaryTree(node1))
+    s = Solution()
+    print(s.invertTree(node1))
