e9d5e4c5e68dd807cd3e5660f13bda6a76e659bb
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-03-08 12:12:54 -0600

    Solve trapping rain water.

---
+++ arrays/trappingRainWater.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trappingRainWater(self, height):
+        if not height: return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
+
+
+
+
+if __name__ == '__main__':
+    heights = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    s = Solution()
+    print(s.trappingRainWater(heights))
