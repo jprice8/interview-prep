85863d6c84acd4ef69cc965b1f03719b9c44a40b
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-04-02 17:48:34 -0500

    Solve reorganize string.

--- heaps/movingBest/reorganizeString.py
+++ heaps/movingBest/reorganizeString.py
@@ -1,19 +1,43 @@
 from collections import Counter
+import heapq
 
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
         n = len(s)
-        # count up characters that appear in the string
+        # count up chars in s
         str_count = Counter(s)
-        # store negative value since we want a max heap
-        pq = [(-value, key) for key, value in enumerate(s)]
-        # return empty string if there are too many of one character
-        if - pq[0][0] > (n + 1) // 2:
+        # no of evens
+        number_of_even_indices = (n + 1) // 2
+        # prepare max heap of counts
+        max_heap = []
+        for char, char_count in str_count.items():
+            heapq.heappush(max_heap, (-char_count, char))
+
+        # is it possible to reorg string?
+        if -max_heap[0][0] > number_of_even_indices:
             return ""
+
         # stores the resulting char array to be converted to string
         res = [None] * n
+        """
+        Pointer to the next item to be inserted.
+        Increment by 2 until it reaches the end to fill out even positions,
+        then it is reset to 1 to fill out odd positions.
+        """
+        pointer = 0
+        # Insert chars into the char array by their multiplicity.
+        while max_heap:
+            (count, char) = heapq.heappop(max_heap)
+            count = -count 
+            for i in range(count):
+                res[pointer] = char
+                pointer += 2
+                if pointer >= n:
+                    pointer = 1
+        return "".join(res)
+
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.reorganizeString("aab"))
+    print(s.reorganizeString("abcaa"))
--- heaps/topK/kthSmallestElementInSortedMatrix.py
+++ heaps/topK/kthSmallestElementInSortedMatrix.py
@@ -1,7 +1,63 @@
+import heapq
+from typing import List
+
+
 class Solution:
-    def example(self):
-        pass
+    # O(n^2*log(k)) time | O(k) space
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        max_heap = []
+        n = len(matrix)
+        for idx in range(n ** 2):        
+            row = idx // n
+            col = idx % n
+            val = matrix[row][col]
+            
+            # Only poll if heap is at or past k
+            if len(max_heap) >= k and val < -max_heap[0]:
+                heapq.heappop(max_heap)
+                heapq.heappush(max_heap, -val)
+            
+            # Always insert if heap not at k
+            if len(max_heap) < k:
+                heapq.heappush(max_heap, -val)
+            
+        return -max_heap[0]
+
+    # O(X + klog(X)) time | O(X) space - where X is min(K, N)
+    def lcHeap(self, matrix, k):
+        # The size of the matrix
+        N = len(matrix)
+
+        # prepare the min-heap
+        min_heap = []
+        for r in range(min(k, N)):
+
+            # add triplets of info for each cell
+            min_heap.append((matrix[r][0], r, 0))
+
+        # heapify the list
+        heapq.heapify(min_heap)
+
+        # until we find k elements
+        while k:
+            # Extract min
+            element, r, c = heapq.heappop(min_heap)
+
+            # if we have any new elements in the current row, add them
+            if c < N - 1:
+                heapq.heappush(min_heap, (matrix[r][c+1], r, c + 1))
+
+            # decrement k
+            k -= 1
+
+        return element
+
 
 if __name__ == '__main__':
+    matrix = [
+        [1, 5, 9],
+        [10, 11, 13],
+        [12, 13, 15]
+    ]
     s = Solution()
-    print(s.example())
+    print(s.lcHeap(matrix, 8))
