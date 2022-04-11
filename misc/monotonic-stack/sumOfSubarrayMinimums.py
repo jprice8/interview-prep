87a432568ead2022bd8d2b31f9d0e1059272306c
Jackson Price <59149601+jprice8@users.noreply.github.com>
2022-04-11 18:26:47 -0500

    Solve sum subarray problems.

--- misc/monotonic-stack/sumOfSubarrayMinimums.py
+++ misc/monotonic-stack/sumOfSubarrayMinimums.py
@@ -2,17 +2,92 @@ from typing import List
 
 
 class Solution:
-    def sumOfSub(self, arr: List[int]) -> int:
+    def sumOfSub(self, weights: List[int]) -> int:
         totalSum = 0
-
+        currSum = 0
         stack = []
-        for num in arr:
-            while stack and stack[-1] > num:
-                stack.pop()
-            stack.append(num)
+        for num in weights:
+            currCount = 1
+
+            # handle increasing stack
+            while stack:
+                if stack[-1][0] < num:
+                    break
+                value, count = stack.pop()
+                currCount += count 
+                currSum -= value * count
+
+            stack.append((num, currCount))
+            currSum += num * currCount
+            totalSum += currSum
 
         return totalSum
 
+    def simpleStack(self, weights):
+        res = 0
+        stack = [] # non-decreasing
+        weights = [float('-inf')] + weights + [float('-inf')]
+        for idx, weight in enumerate(weights):
+            while stack and weights[stack[-1]] > weight:
+                curr = stack.pop()
+                res += weights[curr] * (idx - curr) * (curr - stack[-1])
+            stack.append(idx)
+
+        return res
+    
+    def doublePass(self, weights):
+        previousLess = []
+        nextLess = []
+        leftDistance = []
+        rightDistance = []
+
+        # left distance
+        for idx in range(len(weights)):
+            currWeight = weights[idx]
+            while previousLess and previousLess[-1][0] >= currWeight:
+                previousLess.pop()
+
+            if not previousLess:
+                leftDistance[idx] = idx + 1
+            else:
+                leftDistance[idx] = idx - previousLess[0][1]
+
+            previousLess.append([weights[idx], idx])
+
+        # right distance
+        for idx in reversed(range(len(weights))):
+            currWeight = weights[idx]
+            while nextLess and nextLess[-1][0] > currWeight:
+                nextLess.pop()
+
+            if not nextLess:
+                rightDistance[idx] = len(weights) - 1
+            else:
+                nextLess[0][1] - idx
+
+            nextLess.append([weights[idx], idx])
+
+        ans = 0
+        for idx in range(len(weights)):
+            ans = ans + weights[idx] * leftDistance[idx] * rightDistance[idx]
+        return ans
+
+    def conciseSolution(self, weights):
+        weights.append(-1)
+        stack = [-1]
+        res = 0
+        for idx in range(len(weights)):
+            while weights[idx] < weights[stack[-1]]:
+                curr = stack.pop()
+                res += weights[curr] * (idx - curr) * (curr - stack[-1])
+            stack.append(idx)
+        return res
+    #591340BR
+
+
 if __name__ == '__main__':
     s = Solution()
-    print(s.sumOfSub([1, 3, 2]))
+    # print(s.betterSolution([1, 3, 2]))
+    # print(s.simpleStack([3, 1, 2, 4]))
+    # print(s.betterSolution([1, 3, 2]))
+    print(s.conciseSolution([1, 3, 2]))
--- misc/monotonic-stack/sumOfSubarrayRanges.py
+++ misc/monotonic-stack/sumOfSubarrayRanges.py
@@ -11,6 +11,31 @@ class Solution:
             # update max
             return
 
+    def lcSolution(self, numbers):
+        res = 0
+        inf = float('inf')
+        nums = [-inf] + numbers + [-inf]
+        stack = []
+        # always increasing (min stack)
+        for idx, num in enumerate(nums):
+            while stack and nums[stack[-1]] > num:
+                curr = stack.pop()
+                stackTop = stack[-1]
+                res -= nums[curr] * (curr - stackTop) * (idx - curr) # curr * left distance * right distance
+            stack.append(idx)
+
+        nums = [inf] + numbers + [inf]
+        stack = []
+        # always decreasing (max stack)
+        for idx, num in enumerate(nums):
+            while stack and nums[stack[-1]] < num:
+                curr = stack.pop()
+                stackTop = stack[-1]
+                res += nums[curr] * (curr - stackTop) * (idx - curr) # curr * left distance * right distance
+            stack.append(idx)
+
+        return res
+
 if __name__ == '__main__':
     s = Solution()
-    print(s.sumOfRanges([1, 2, 3]))
+    print(s.lcSolution([1, 2, 3]))
